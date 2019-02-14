a0986adcb5c4db2a0ecfec62c46814a29d66bb66
first773 <first773@163.com>
2019-02-14 23:16:42 +0800

    'add-page'

--- src/components/content/rightSection/RightSectionPage.js
+++ src/components/content/rightSection/RightSectionPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import page1 from '../../../static/images/design.png'
+import page2 from '../../../static/images/test/page2.jpg'
+
 class RightSectionPage extends Component {
     render() {
         return (
             <div className="right-section-page">
               <div className="right-preview">
+                <div className="page page-selected">
+                    <img src={page1}></img>
+                </div>
                 <div className="page">
-                    <img></img>
+                    <img src={page2}></img>
                 </div>
               </div>
             </div>
